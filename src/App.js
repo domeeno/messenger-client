766a0965c255422f7a262d5fe453894b8dacf354
dominatorfaf <dominicflocea.dev@gmail.com>
2020-12-05 22:47:58 +0200

    Cleaned code. Made a basic input app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
+import React, {useState, useCallback, useEffect} from 'react'
 
-function App() {
+const App = () =>  {
+  //updating contents of the message input as user types.
+  const [messageTextBox, setMessageTextBox] = useState('')
+  const [listOfMessages, setListOfMessages] = useState([])
+
+  const onMessageInputChange = useCallback((event) => {
+    console.log(event.target.value)
+    setMessageTextBox(event.target.value)
+  }, [])
+
+  const sendMessage = useCallback((event) => {
+    event.preventDefault()
+    //print user that sent the message
+    setListOfMessages([
+      ...listOfMessages, 
+      {
+        id: listOfMessages.length + 1,
+        content: messageTextBox,
+        done: false,
+      }
+    ])
+
+    setMessageTextBox("")
+  }, [listOfMessages, messageTextBox])
+
+  useEffect(() => {
+    console.log(listOfMessages.length)
+  }, [listOfMessages])
+
   return (
     <div className="App"> 
-      <h1>Hello World</h1>
+      <ul>
+        {listOfMessages.map((message) => (
+          <li key={message.id} >{message.content}</li>
+        ))}
+      </ul>
+      <form onSubmit={sendMessage}>
+        <input onChange={onMessageInputChange} 
+        placeholder="message"  
+        id="msg"
+        type="text"
+        value={messageTextBox}
+        // just ignore for the future input like = ""
+        required 
+        />
+        <button className="btn">Send</button>
+      </form>
     </div>
   ); 
 }
