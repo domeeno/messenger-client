18e0ca247bfe96b5da3bd78067be748b8ec6978b
dominatorfaf <dominicflocea.dev@gmail.com>
2020-12-06 13:33:09 +0200

    Refactored default

--- src/API.js
+++
@@ -1,6 +0,0 @@
-const API_URL = 'http://localhost:8080'
-
-export async function fetchMessages () {
-    const response = await fetch(`${API_URL}/api/messages`)
-    return response.json()
-}
--- src/App.js
+++ src/App.js
@@ -1,62 +1,8 @@
-import React, {useState, useCallback, useEffect} from 'react'
-
-import { fetchMessages } from './API'
+import React from 'react'
+import Chat from './components/Chat'
 
 const App = () =>  {
-  //updating contents of the message input as user types.
-  const [messageTextBox, setMessageTextBox] = useState('')
-  const [listOfMessages, setListOfMessages] = useState([])
-
-  const onMessageInputChange = useCallback((event) => {
-    console.log(event.target.value)
-    setMessageTextBox(event.target.value)
-  }, [])
-
-  const sendMessage = useCallback((event) => {
-    event.preventDefault()
-    //print user that sent the message
-    setListOfMessages([
-      ...listOfMessages, 
-      {
-        id: listOfMessages.length + 1,
-        content: messageTextBox,
-        done: false,
-      }
-    ])
-
-    setMessageTextBox("")
-  }, [listOfMessages, messageTextBox])
-
-  useEffect(() => {
-    (async () => {
-      const listOfFetchedMessages = await fetchMessages()
-      console.log(listOfFetchedMessages)
-    })();
-    console.log(listOfMessages.length)
-  }, [listOfMessages])
-
-
-
-  return (
-    <div className="App"> 
-      <ul>
-        {listOfMessages.map((message) => (
-          <li key={message.id} >{message.content}</li>
-        ))}
-      </ul>
-      <form onSubmit={sendMessage}>
-        <input onChange={onMessageInputChange} 
-        placeholder="message"  
-        id="msg"
-        type="text"
-        value={messageTextBox}
-        // just ignore for the future input like = ""
-        required 
-        />
-        <button className="btn">Send</button>
-      </form>
-    </div>
-  ); 
+  return <Chat />
 }
 
 export default App;
